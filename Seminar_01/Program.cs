49a4d22bf6194d9a3ec71ee3fdc9685d10be3a88
Александр <popovaleksandr201306@gmail.com>
2022-08-15 19:47:37 +0500

    Работа во время семинара

---
+++ Seminar_01/Program.cs
@@ -0,0 +1,112 @@
+﻿
+Console.Clear();
+
+// 1 Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+
+// Console.Write("Введите число: ");
+
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int sqrt = number * number;  // number *= number;
+// int sqrt1 = Convert.ToInt32(Math.Pow(number, 2));
+
+// Console.WriteLine("Число в квадрате: " + sqrt1);
+
+
+// задача 2
+
+// 2  Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+
+// Console.Write("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if (num1 == num2 * num2)
+// {
+//     Console.Write("Первое число - это квадрат второго числа");
+// }
+// else
+// {
+//     Console.Write("Первое число не является квадратом второго числа");
+// }
+
+
+// задача 3
+
+// / 3 Напишите программу, которая будет выдавать название дня недели по заданному номеру
+
+// Console.Write("Введите номер дня: ");
+
+// int day = Convert.ToInt32(Console.ReadLine());
+
+// if (day < 1 || day > 7)
+// {
+//     Console.Write("Неправильный номер");
+// }
+// else
+// {
+//     if (day == 1)
+//     {
+//         Console.Write("Понедельник");
+//     }
+//     if (day == 2)
+//     {
+//         Console.Write("Вторник");
+//     }
+//     if (day == 3)
+//     {
+//         Console.Write("Среда");
+//     }
+//     if (day == 4)
+//     {
+//         Console.Write("Четверг");
+//     }
+//     if (day == 5)
+//     {
+//         Console.Write("Пятница");
+//     }
+//     if (day == 6)
+//     {
+//         Console.Write("Суббота");
+//     }
+//     if (day == 7)
+//     {
+//         Console.Write("Воскресенье");
+//     }
+// }
+
+
+// Задача 4
+
+
+// 4 Напишите программу, которая на вход принимает одно число (N), 
+//а на выходе показывает все целые числа в промежутке от -N до N.
+
+// Console.Write("Введите число: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int B = -N;
+// while (B <= N)
+// {
+//     Console.Write(B + " ");
+//     B++; // B = B + 1 аналогично B += 1 
+// }
+
+
+// Задача 5
+
+
+// 5 Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+// Console.Write("Введите трехзначное число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int y = num % 10;
+
+// Console.WriteLine(y);
+
+
+
+
+// условие для чётных number % 2 == 0
+
+
